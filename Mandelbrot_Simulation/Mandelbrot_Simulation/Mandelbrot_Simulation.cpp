9be49f500e4489b37f2a3f2acc8a5239addd235a
OdegaOmie <of78298@yahoo.com>
2017-11-12 21:09:40 +0000

    Mandelbrot visible# complete

--- Mandelbrot_Simulation/Mandelbrot_Simulation/Mandelbrot_Simulation.cpp
+++ Mandelbrot_Simulation/Mandelbrot_Simulation/Mandelbrot_Simulation.cpp
@@ -1,11 +1,95 @@
-// Mandelbrot_Simulation.cpp : Defines the entry point for the console application.
-//
+//////////////////////////////////////////////////////////// 
+// Headers 
+//////////////////////////////////////////////////////////// 
+#include "stdafx.h" 
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+#pragma comment(lib,"opengl32.lib") 
+#pragma comment(lib,"glu32.lib") 
 
-#include "stdafx.h"
+
+#include <iostream> 
+#include <vector>
+#include "SFML/Graphics.hpp" 
+#include "SFML/OpenGL.hpp" 
+#include <complex>
+#include "MandelBrotGenerator.h"
+
+int width = 400;
+int height = 400;
+float zoom = 1;
+
 
 
 int main()
 {
-    return 0;
-}
 
+
+	sf::RenderWindow App(sf::VideoMode(width, height, 32), "SFML OpenGL");
+
+
+	MandelBrotGenerator mandel = MandelBrotGenerator(width, height, zoom);
+
+
+
+	glClearDepth(1.f);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.f); //background colour
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
+
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+
+
+	// Start game loop 
+	while (App.isOpen())
+	{
+		// Process events 
+		sf::Event Event;
+		while (App.pollEvent(Event))
+		{
+			// Close window : exit 
+			if (Event.type == sf::Event::Closed)
+				App.close();
+
+			// Escape key : exit 
+			if ((Event.type == sf::Event::KeyPressed) && (Event.key.code == sf::Keyboard::Escape))
+				App.close();
+		}
+
+		//Prepare for drawing 
+		// Clear color and depth buffer 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Apply some transformations 
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		App.clear(sf::Color::Black);
+		if (!mandel.getReady()) {
+			mandel.run();
+		}
+		
+
+
+		///HERE BE STUFF////
+		App.draw(mandel.getSprite());
+
+		App.display();
+	}
+
+	return EXIT_SUCCESS;
+}
